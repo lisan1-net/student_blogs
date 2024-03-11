d192bc0ac60873283bd8e6895900715662f817da
Hamza <hamza.abbad@gmail.com>
2024-03-11 17:58:15 +0800

    Added the models to the admin site.

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from main.models import *
 
-admin.register(Blog)
-admin.register(Author)
-admin.register(Text)
+admin.site.register(Blog)
+admin.site.register(Author)
+admin.site.register(Text)
