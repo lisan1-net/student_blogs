10540c10fae12f6d26c84a11fbe4a5a3533578e1
Hamza <hamza.abbad@gmail.com>
2024-03-11 15:40:35 +0800

    Use PostgreSQL.

--- database_search_engine/settings.py
+++ database_search_engine/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'database_search_engine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'database_search_engine'),
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
