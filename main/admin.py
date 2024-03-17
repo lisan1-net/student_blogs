26043a7baf1f0d869437e1a4024fbf4468d0a9dd
Hamza <hamza.abbad@gmail.com>
2024-03-17 11:43:06 +0800

    Use the app name in the admin site title and header.

--- main/admin.py
+++ main/admin.py
@@ -4,3 +4,6 @@ from django.apps import apps
 app = apps.get_app_config('main')
 for model in app.get_models():
     admin.site.register(model)
+
+admin.site.site_header = app.verbose_name
+admin.site.site_title = app.verbose_name
