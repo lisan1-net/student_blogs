6470654e76998dfe77309ee1e8f19d50a32cc37f
Hamza <hamza.abbad@gmail.com>
2024-03-20 20:06:16 +0800

    Added health check view.

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('text/<int:pk>/', views.text, name='text'),
     path('words/', views.vocabulary, name='vocabulary'),
     path('search_widget/', views.search_widget, name='search_widget'),
+    path('healthz/', views.health_check, name='health_check'),
     path('', views.home, name='home')
 ]
