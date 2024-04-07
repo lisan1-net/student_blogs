4220a0879fa1c1f00e887abfe46cb55140418a17
Hamza <hamza.abbad@gmail.com>
2024-04-07 10:30:15 +0800

    Convert the migration into a command to be launched as a job.

---
+++ indexes/management/commands/migratetokens.py
@@ -0,0 +1,44 @@
+from django.apps import apps
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = 'Migrate TextWords to TextTokens and Words to Tokens'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--max-instances', type=int, default=0, help='Maximum number of instances to migrate')
+
+    def handle(self, *args, **options):
+        TextWord = apps.get_model('indexes', 'TextWord')
+        Token = apps.get_model('indexes', 'Token')
+        TextToken = apps.get_model('indexes', 'TextToken')
+        start = 0
+        end = 0
+        token_content = ''
+        token_index = 1
+        text_words = TextWord.objects.exclude(
+            text__in=TextToken.objects.values('text'), start__in=TextToken.objects.values('start'),
+            end__in=TextToken.objects.values('end')
+        )
+        if options['max_instances']:
+            text_words = text_words[:options['max_instances']]
+        self.stdout.write('Number of text words to migrate: %d' % text_words.count())
+        text_word = None
+        for word_index, text_word in enumerate(text_words, 1):
+            self.stdout.write('Migrating text word %d: %s' % (word_index, str(text_word)))
+            if text_word.start == start and text_word.end == end:
+                token_content += text_word.word.content
+            elif token_content:
+                token, created = Token.objects.get_or_create(content=token_content)
+                text_token, created = TextToken.objects.get_or_create(text=text_word.text, token=token, start=start, end=end)
+                self.stdout.write('Migrated text token %d: %s' % (token_index, str(text_token)))
+                token_index += 1
+                token_content = text_word.word.content
+            start = text_word.start
+            end = text_word.end
+        if token_content and text_word:
+            token, created = Token.objects.get_or_create(content=token_content)
+            text_token, created = TextToken.objects.get_or_create(text=text_word.text, token=token, start=start, end=end)
+            self.stdout.write('Migrated text token %d: %s' % (token_index, str(text_token)))
+        self.stdout.write(self.style.SUCCESS('Finished migrating text tokens'))
