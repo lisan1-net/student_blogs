96ae20196eeb88ce2382071debeb6602ae8f012f
Hamza <hamza.abbad@gmail.com>
2024-04-07 11:17:26 +0800

    Improve the admin interface for blogs and texts.

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,28 @@
-from django.contrib import admin
 from django.apps import apps
+from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+
+from main.models import Blog, Text
 
 ModelAdmin.list_per_page = 50
 
 app = apps.get_app_config('main')
-for model in app.get_models():
-    admin.site.register(model)
-
 admin.site.site_header = app.verbose_name
 admin.site.site_title = app.verbose_name
+
+
+@admin.register(Blog)
+class BlogAdmin(ModelAdmin):
+
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title', 'type', 'student_number', 'sex', 'level', 'school', 'city', 'author_name', 'source_type',
+    )
+    list_filter = tuple(e for e in list_display if e not in ('author_name', 'title'))
+    search_fields = ('title', 'content', 'school', 'city', 'author_name')
