436333be88b997056841f4e09c5182025b083532
Hamza <hamza.abbad@gmail.com>
2024-04-08 13:23:46 +0800

    Added `try...catch` for migrating tokens to avoid duplication errors.

--- indexes/management/commands/migratetokens.py
+++ indexes/management/commands/migratetokens.py
@@ -1,5 +1,6 @@
 from django.apps import apps
 from django.core.management import BaseCommand
+from django.db.utils import IntegrityError
 
 
 class Command(BaseCommand):
@@ -30,15 +31,21 @@ class Command(BaseCommand):
             if text_word.start == start and text_word.end == end:
                 token_content += text_word.word.content
             elif token_content:
-                token, created = Token.objects.get_or_create(content=token_content)
-                text_token, created = TextToken.objects.get_or_create(text=text_word.text, token=token, start=start, end=end)
-                self.stdout.write('Migrated text token %d: %s' % (token_index, str(text_token)))
+                try:
+                    token, created = Token.objects.get_or_create(content=token_content)
+                    text_token, created = TextToken.objects.get_or_create(text=text_word.text, token=token, start=start, end=end)
+                    self.stdout.write('Migrated text token %d: %s' % (token_index, str(text_token)))
+                except IntegrityError:
+                    self.stdout.write(self.style.ERROR('Failed to migrate text token %d: %s' % (token_index, token_content)))
                 token_index += 1
                 token_content = text_word.word.content
             start = text_word.start
             end = text_word.end
         if token_content and text_word:
-            token, created = Token.objects.get_or_create(content=token_content)
-            text_token, created = TextToken.objects.get_or_create(text=text_word.text, token=token, start=start, end=end)
-            self.stdout.write('Migrated text token %d: %s' % (token_index, str(text_token)))
+            try:
+                token, created = Token.objects.get_or_create(content=token_content)
+                text_token, created = TextToken.objects.get_or_create(text=text_word.text, token=token, start=start, end=end)
+                self.stdout.write('Migrated text token %d: %s' % (token_index, str(text_token)))
+            except IntegrityError:
+                self.stdout.write(self.style.ERROR('Failed to migrate text token %d: %s' % (token_index, token_content)))
         self.stdout.write(self.style.SUCCESS('Finished migrating text tokens'))
